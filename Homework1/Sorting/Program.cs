8b71cce3eba70362db7af8b2979e20f78c3d29f2
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-02-25 15:19:06 +0300

    Initial commit (program without sorting function and tests)

---
+++ Homework1/Sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Enter elements of array: ");
+            var sequenceOfNumbers = Console.ReadLine();
+            if (sequenceOfNumbers == null)
+            {
+                Console.WriteLine("No elements for sorting");
+                return 0;
+            }
+            int[] arrayOfNumbers = makeArrayFromString(sequenceOfNumbers);
+
+            return 0;
+        }
+        static int[] makeArrayFromString(string sequenceOfNumbers)
+        {
+            string[] stringArrayOfNumbers = sequenceOfNumbers.Split(' ');
+            int[] intArrayOfNumbers = new int[stringArrayOfNumbers.Length];
+            for (int i = 0; i < stringArrayOfNumbers.Length; ++i)
+            {
+                intArrayOfNumbers[i] = int.Parse(stringArrayOfNumbers[i]);
+            }
+            return intArrayOfNumbers;
+        }
+        static void sortByInsertion(int[] arrayForSorting)
+        {
+            
+        }
+    }
+}
