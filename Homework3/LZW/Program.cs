51fbe5e08806c806f543e6d9be2ec4e408733159
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-03-17 11:52:24 +0300

    Initial commit

---
+++ Homework3/LZW/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LZW;
+using System;
+using System.Collections;
+public class LZW
+{
+    private static void Main(string[] args)
+    {
+        byte[] arrayOfBytes
+            = File.ReadAllBytes(@"C:\Users\Home\source\repos\Space-for-CSharp-development\Homework3\ExampleDocument.txt");
+        BitArray bits = new BitArray(arrayOfBytes.Length * 8);
+    }
+}
