7fa0c877c8ea68203461397cd3e17701c740ad71
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-03-17 20:18:59 +0300

    In process (before merge "Trie" in "LWZ")

--- Homework3/LZW/Program.cs
+++ Homework3/LZW/Program.cs
@@ -1,12 +1,22 @@
 ﻿namespace LZW;
 using System;
 using System.Collections;
+using System.IO;
 public class LZW
 {
+    public static bool Zip(BitArray bitsFromFile, string fileNameForZipping)
+    {
+        FileInfo newFile = new(fileNameForZipping + ".zipped");
+        FileStream zippedFile = newFile.Create();
+        return true;
+    }
+
     private static void Main(string[] args)
     {
         byte[] arrayOfBytes
-            = File.ReadAllBytes(@"C:\Users\Home\source\repos\Space-for-CSharp-development\Homework3\ExampleDocument.txt");
-        BitArray bits = new BitArray(arrayOfBytes.Length * 8);
+            = File.ReadAllBytes("..\\..\\..\\ExampleDocument.txt");
+        BitArray bitsFromFile = new BitArray(arrayOfBytes);
+        BitArray zippedBits = new BitArray(64);
+        bool result = LZW.Zip(bitsFromFile, "..\\..\\..\\hahaha.txt");
     }
 }
