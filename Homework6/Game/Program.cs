e64b0087d2b807170f094d9c8a0f80eb9f587b4d
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-04-13 19:42:19 +0300

    Good-working task without tests

---
+++ Homework6/Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using Game;
+
+var eventLoop = new EventLoop();
+var game = new Game.Game();
+
+var pathToFile = "C:\\Users\\Home\\source\\repos\\Space-for-CSharp-development\\Homework6\\Game\\Map.txt";
+game.CreateMatrixFromFileMap(pathToFile);
+game.PrintMapMatrix();
+
+eventLoop.LeftHandler += game.OnLeft;
+eventLoop.RightHandler += game.OnRight;
+eventLoop.TopHandler += game.Up;
+eventLoop.BottomHandler += game.Down;
+
+
+eventLoop.Run();
